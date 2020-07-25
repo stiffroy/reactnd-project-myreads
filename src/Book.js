5af1f46f5264f09ef22cb995686b0d79fb9e4ddf
stiff.roy <stiff.roy@gmail.com>
2020-07-25 23:30:14 +0200

    [FIX] Showing a fallback image in case the book don't have a thumbnail

--- src/Book.js
+++ src/Book.js
@@ -8,12 +8,13 @@ class Book extends React.Component{
     }
     render() {
         const {book, onShelfUpdate} = this.props
+        const image = book.imageLinks ? book.imageLinks.thumbnail : 'https://dummyimage.com/128x193&text=No+Image'
 
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
                         <div className="book-shelf-changer">
                             <select
                                 value={book.shelf ? book.shelf : 'none'}
