bdf969bfe345f4144530aac5fe9c6e0fec208a13
stiff.roy <stiff.roy@gmail.com>
2020-07-26 18:48:01 +0200

    [TASK] Converting the components to stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-class Book extends React.Component{
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onShelfUpdate: PropTypes.func.isRequired,
-    }
-    render() {
-        const {book, onShelfUpdate} = this.props
-        const image = book.imageLinks ? book.imageLinks.thumbnail : 'https://dummyimage.com/128x193&text=No+Image'
+const Book = (props) => {
+    const {book, onShelfUpdate} = props
+    const image = book.imageLinks ? book.imageLinks.thumbnail : 'https://dummyimage.com/128x193&text=No+Image'
 
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select
-                                value={book.shelf ? book.shelf : 'none'}
-                                onChange={(event) => onShelfUpdate(book, event.target.value)}
-                            >
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">
-                        {book.authors && book.authors.map(author => (
-                            <span key={author}>{author} <br/></span>
-                        ))}
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
+                    <div className="book-shelf-changer">
+                        <select
+                            value={book.shelf ? book.shelf : 'none'}
+                            onChange={(event) => onShelfUpdate(book, event.target.value)}
+                        >
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
                 </div>
-            </li>
-        )
-    }
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                    {book.authors && book.authors.map(author => (
+                        <span key={author}>{author} <br/></span>
+                    ))}
+                </div>
+            </div>
+        </li>
+    )
+}
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    onShelfUpdate: PropTypes.func.isRequired,
 }
 
 export default Book
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,69 +2,67 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Book from "./Book";
 
-class ListBooks extends React.Component{
-    render() {
-        let currentlyReading = this.props.books.filter(book => book.shelf === "currentlyReading")
-        let wantToRead = this.props.books.filter(book => book.shelf === "wantToRead")
-        let read = this.props.books.filter(book => book.shelf === "read")
+const ListBooks = (props) => {
+    let currentlyReading = props.books.filter(book => book.shelf === "currentlyReading")
+    let wantToRead = props.books.filter(book => book.shelf === "wantToRead")
+    let read = props.books.filter(book => book.shelf === "read")
 
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    { currentlyReading.map(book => (
-                                        <Book
-                                            key={book.id}
-                                            book={book}
-                                            onShelfUpdate={this.props.onShelfUpdate}
-                                        />
-                                    )) }
-                                </ol>
-                            </div>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Currently Reading</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                { currentlyReading.map(book => (
+                                    <Book
+                                        key={book.id}
+                                        book={book}
+                                        onShelfUpdate={props.onShelfUpdate}
+                                    />
+                                )) }
+                            </ol>
                         </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    { wantToRead.map(book => (
-                                        <Book
-                                            key={book.id}
-                                            book={book}
-                                            onShelfUpdate={this.props.onShelfUpdate}
-                                        />
-                                    )) }
-                                </ol>
-                            </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Want to Read</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                { wantToRead.map(book => (
+                                    <Book
+                                        key={book.id}
+                                        book={book}
+                                        onShelfUpdate={props.onShelfUpdate}
+                                    />
+                                )) }
+                            </ol>
                         </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    { read.map(book => (
-                                        <Book
-                                            key={book.id}
-                                            book={book}
-                                            onShelfUpdate={this.props.onShelfUpdate}
-                                        />
-                                    )) }
-                                </ol>
-                            </div>
+                    </div>
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                        <div className="bookshelf-books">
+                            <ol className="books-grid">
+                                { read.map(book => (
+                                    <Book
+                                        key={book.id}
+                                        book={book}
+                                        onShelfUpdate={props.onShelfUpdate}
+                                    />
+                                )) }
+                            </ol>
                         </div>
                     </div>
                 </div>
-                <div className="open-search">
-                    <Link to='/search'>Search a book</Link>
-                </div>
             </div>
-        )
-    }
+            <div className="open-search">
+                <Link to='/search'>Search a book</Link>
+            </div>
+        </div>
+    )
 }
 
 export default ListBooks
